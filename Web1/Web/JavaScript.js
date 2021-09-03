c76df2905dbc5f04d3c08b614d72b0c09d2819c9
kouki1311 <koshige25@gmail.com>
2021-09-03 10:11:09 +0900

    削除と更新と新規追加

---
+++ Web1/Web/JavaScript.js
@@ -0,0 +1,16 @@
+﻿const pika = document.getElementById('Delete');
+
+
+
+
+let button = document.getElementById('Delete');
+button.addEventListener('click', function () {
+
+    alert(2);
+})
+
+
+button.addEventListener('click', butotnClick);
+function butotnClick(event) {
+    alert(2);
+}
